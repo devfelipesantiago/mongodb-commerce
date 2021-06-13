90e89655cc36082acaac0a8c52bd7676b54f8642
Felipe <felipesantiagolds@gmail.com>
2021-06-13 19:47:02 -0300

    Commit full project

---
+++ challenges/desafio19.js
@@ -0,0 +1,12 @@
+/*
+Crie uma query que faça a renomeação do campo descricao para descricaoSite em todos os documentos.
+Crie uma query que retorne o nome, descricao e descricaoSite de todos os documentos.
+*/
+
+db.produtos.updateMany({},
+  {
+    $rename:
+      { descricao: "descricaoSite" },
+  });
+
+db.produtos.find({}, { _id: false, nome: true, descricao: true, descricaoSite: true });
---
+++ challenges/desafio2.js
@@ -0,0 +1,6 @@
+db.produtos.updateMany(
+  {},
+  { $set: { valorUnitario: NumberDecimal("0.00") } }, { upsert: true },
+);
+
+db.produtos.find({}, { _id: false, nome: true, valorUnitario: true });
